0eeb1edd3f3e300290b1d261753f5af5aec4fe90
Leonid M <leonidms@gmail.com>
2010-12-08 00:14:05 +0100

    Initial commit

---
+++ propertyset-storage-toolkit-sample/src/test/java/com/atlassian/plugins/studio/propertyset/storage/ExampleServletTest.java
@@ -0,0 +1,15 @@
+package com.atlassian.plugins.studio.propertyset.storage;
+
+import org.junit.Test;
+
+/**
+ * Testing {@link com.atlassian.plugins.studio.propertyset.storage.ExampleServlet}
+ */
+public class ExampleServletTest
+{
+    @Test
+    public void basic()
+    {
+        // add test here...
+    }
+}
